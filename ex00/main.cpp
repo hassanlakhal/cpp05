ae67f498b35d27f0c645230c6fa2fdb5feaa0d21
Hassan Lakhal <hlakhal-@e1r4p8.1337.ma>
2023-11-14 01:49:34 +0100

    fixed exercice 00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 21:56:49 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/11/14 01:33:29 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/11/14 01:46:51 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,18 @@ int main()
         Bureaucrat obj("GradeHigh", 150);
         obj.decrement();
         std::cout << obj << std::endl;
+        Bureaucrat obj1("GradeLow", 1);
+        obj1.increment();
+        std::cout << obj1 << std::endl;
     }
-    catch (std::exception& e)
+    catch (Bureaucrat::GradeTooHighException& e)
     {
         std::cerr << e.what() << std::endl;
     }
-
+    catch (Bureaucrat::GradeTooLowException& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
     return 0;
 }
 
