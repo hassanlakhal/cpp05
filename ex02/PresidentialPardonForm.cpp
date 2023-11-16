17858b8e041a63a242cf11f45e21ca7e98cf9157
Hassan Lakhal <hlakhal-@e1r3p10.1337.ma>
2023-11-16 03:20:41 +0100

    fixed class ShrubberyCreationForm

--- ex02/PresidentialPardonForm.cpp
+++ ex02/PresidentialPardonForm.cpp
@@ -6,7 +6,7 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 22:11:10 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/11/16 01:30:28 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/11/16 03:20:34 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ PresidentialPardonForm::PresidentialPardonForm()
     
 }
 
-PresidentialPardonForm::PresidentialPardonForm(std::string& target):Form(target,72,45)
+PresidentialPardonForm::PresidentialPardonForm(std::string& target):Form(target,25,5)
 {
     
 }
