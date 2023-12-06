0ceca505d291c700f1ffc8cb54c5ac1562bce914
Hassan Lakhal <hlakhal-@e1r3p9.1337.ma>
2023-12-06 10:17:12 +0100

    done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 21:56:49 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/12/05 10:01:46 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/12/06 10:16:26 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,8 @@ int main()
     try
     {
         Bureaucrat obj("GradeHigh", 1);
-        // obj = obj1;
         Form form("form",10,2);
-        // Form form1("test",150,2);
         obj.signForm(form); 
-      
     }
     catch (std::exception& e)
     {
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 21:56:49 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/12/02 17:28:00 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/12/06 10:07:26 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,6 @@ int main()
 {
     try
     {
-        Bureaucrat bureaucrat("John Doe", 1);
-        AForm *a[3] = {
-                        new ShrubberyCreationForm("home"),
-                        new RobotomyRequestForm("office"),
-                        new PresidentialPardonForm("friend")
-                     };
-        for (int i = 0; i < 3; i++)
-        {
-            bureaucrat.signForm(*a[i]);
-        }
-        a[0]->execute(bureaucrat);
-        a[1]->execute(bureaucrat);
-        a[2]->execute(bureaucrat);
-        std::cout << "\n-----------------------------------\n";
         Bureaucrat burt("Jack_Td", 45);
         ShrubberyCreationForm shrubberyForm("home");
         RobotomyRequestForm robotomyForm("office");
@@ -45,10 +31,6 @@ int main()
         burt.executeForm(shrubberyForm);
         burt.executeForm(robotomyForm);
         burt.executeForm(pardonForm);
-         for (int i = 0; i < 3; i++)
-        {
-            delete a[i];
-        }
     }
     catch (std::exception& e)
     {
